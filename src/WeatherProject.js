ecd5cf80cc4611f2e2ccf1fb969e5a3bf51b8bb8
Sofia Casenave <sofia_casenave@hotmail.com>
2019-11-30 18:52:00 +0000

    Styled the Loader and updated the code for the suggested cities

--- src/WeatherProject.js
+++ src/WeatherProject.js
@@ -23,15 +23,6 @@ export default function WeatherProject(props) {
     });
   }
 
-  function searchCity1(london) {
-    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=London&APPID=3a94f3778290bfeee61278505dbbe51d&units=metric`;
-    axios.get(apiUrl).then(displayTemperature);
-  }
-  function searchCity2(venice) {
-    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Venice,it&APPID=3a94f3778290bfeee61278505dbbe51d&units=metric`;
-    axios.get(apiUrl).then(displayTemperature);
-  }
-
   function handleSubmit(event) {
     event.preventDefault();
     search();
@@ -47,92 +38,157 @@ export default function WeatherProject(props) {
     axios.get(apiUrl).then(displayTemperature);
   }
 
+  function suggestedCity1(city) {
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=London&APPID=3a94f3778290bfeee61278505dbbe51d&units=metric`;
+    axios.get(apiUrl).then(displayTemperature);
+  }
+
+  function suggestedCity2(city) {
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Venice,it&APPID=3a94f3778290bfeee61278505dbbe51d&units=metric`;
+    axios.get(apiUrl).then(displayTemperature);
+  }
+
+  function suggestedCity3(city) {
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=New York&APPID=3a94f3778290bfeee61278505dbbe51d&units=metric`;
+    axios.get(apiUrl).then(displayTemperature);
+  }
+
+  function suggestedCity4(city) {
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Toronto&APPID=3a94f3778290bfeee61278505dbbe51d&units=metric`;
+    axios.get(apiUrl).then(displayTemperature);
+  }
+
+  function suggestedCity5(city) {
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Sydney&APPID=3a94f3778290bfeee61278505dbbe51d&units=metric`;
+    axios.get(apiUrl).then(displayTemperature);
+  }
+
   if (weatherData.ready) {
     return (
-      <div className="WeatherProject">
-        <section className="weatherProject">
-          <div className="row">
-            <div className="col-7">
-              <div className="searchEngine">
-                <form onSubmit={handleSubmit}>
-                  <input
-                    type="search"
-                    placeholder=" Search City"
-                    autoFocus="on"
-                    autoComplete="off"
-                    onChange={handleCityChange}
-                  />
-                  <button type="submit" value="Search">
-                    <i className="fas fa-search" />
-                  </button>
-                  <button type="submit" value="Search">
-                    <i className="fas fa-map-marker-alt" />
-                  </button>
-                </form>
-              </div>
-            </div>
-            <div className="col-5">
-              <h1 className="appName">
-                <strong>horizon|</strong>METEO
-              </h1>
+      <div className="weatherProject">
+        <div className="row">
+          <div className="col-7">
+            <div className="searchEngine">
+              <form onSubmit={handleSubmit}>
+                <input
+                  type="search"
+                  placeholder=" Search City"
+                  autoFocus="on"
+                  autoComplete="off"
+                  onChange={handleCityChange}
+                />
+                <button type="submit" value="Search">
+                  <i className="fas fa-search" />
+                </button>
+                <button type="submit" value="Search">
+                  <i className="fas fa-map-marker-alt" />
+                </button>
+              </form>
             </div>
           </div>
-          <div className="cities">
-            <a href="/" onClick={searchCity1}>
-              London
-            </a>
-            <a href="/" onClick={searchCity2}>
-              Venice
-            </a>
-            <a href="/">New York</a>
-            <a href="/">Toronto</a>
-            <a href="/">Sydney</a>
+          <div className="col-5">
+            <h1 className="appName">
+              <strong>horizon|</strong>METEO
+            </h1>
           </div>
-          <hr className="lineDivider" />
-          <div className="illustration">
-            <img
-              src="https://images.pexels.com/photos/220444/pexels-photo-220444.jpeg?cs=srgb&dl=architecture-buildings-city-220444.jpg&fm=jpg"
-              alt="Man standing on white concrete surface"
-              className="float-right"
-            />
-          </div>
-          <WeatherInfo data={weatherData} />
-          <hr className="lineDivider" />
-          <WeatherForecast city={weatherData.city} />
-          <div className="identification">
-            <footer>
-              GitHub{" "}
+        </div>
+        <div className="cities">
+          <button className="exampleCity" onClick={suggestedCity1}>
+            London
+          </button>
+          <button className="exampleCity" onClick={suggestedCity2}>
+            Venice
+          </button>
+          <button className="exampleCity" onClick={suggestedCity3}>
+            New York
+          </button>
+          <button className="exampleCity" onClick={suggestedCity4}>
+            Toronto
+          </button>
+          <button className="exampleCity" onClick={suggestedCity5}>
+            Sydney
+          </button>
+        </div>
+        <hr className="lineDivider" />
+        <div className="illustration">
+          <img
+            src="https://images.pexels.com/photos/220444/pexels-photo-220444.jpeg?cs=srgb&dl=architecture-buildings-city-220444.jpg&fm=jpg"
+            alt="Man standing on white concrete surface"
+            className="float-right"
+          />
+        </div>
+        <WeatherInfo data={weatherData} />
+        <hr className="lineDivider" />
+        <WeatherForecast city={weatherData.city} />
+        <div className="identification">
+          <footer>
+            GitHub{" "}
+            <a
+              className="githubRepo"
+              href="https://github.com/SofiaCasenave/react_weather_project"
+              target="_blank"
+            >
+              open-sourced
+            </a>{" "}
+            and hosted on{" "}
+            <a
+              href="https://naughty-turing-b5288e.netlify.com/"
+              target="_blank"
+            >
+              Netlify
+            </a>
+            <span className="author">
+              {" "}
+              | 2019 | coded by{" "}
               <a
-                className="githubRepo"
-                href="https://github.com/SofiaCasenave/react_weather_project"
+                href="https://www.linkedin.com/in/sofiacasenave"
                 target="_blank"
               >
-                open-sourced
-              </a>{" "}
-              and hosted on{" "}
-              <a
-                href="https://naughty-turing-b5288e.netlify.com/"
-                target="_blank"
-              >
-                Netlify
+                <strong>Sofia Casenave</strong>
               </a>
-              <span className="author">
-                {" "}
-                | 2019 | coded by{" "}
-                <a
-                  href="https://www.linkedin.com/in/sofiacasenave"
-                  target="_blank"
-                >
-                  <strong>Sofia Casenave</strong>
-                </a>
-              </span>
-            </footer>
-          </div>
-        </section>
+            </span>
+          </footer>
+        </div>
       </div>
     );
   } else {
     search();
-    return <Loader type="Oval" color="#127fce" height={100} width={100} />;
+    return (
+      <div className="weatherProject">
+        <div className="loader">
+          <Loader type="Oval" color="#127fce" height={100} width={100} />
+        </div>
+        <hr className="lineDivider" />
+        <div className="identification">
+          <footer>
+            GitHub{" "}
+            <a
+              className="githubRepo"
+              href="https://github.com/SofiaCasenave/react_weather_project"
+              target="_blank"
+            >
+              open-sourced
+            </a>{" "}
+            and hosted on{" "}
+            <a
+              href="https://naughty-turing-b5288e.netlify.com/"
+              target="_blank"
+            >
+              Netlify
+            </a>
+            <span className="author">
+              {" "}
+              | 2019 | coded by{" "}
+              <a
+                href="https://www.linkedin.com/in/sofiacasenave"
+                target="_blank"
+              >
+                <strong>Sofia Casenave</strong>
+              </a>
+            </span>
+          </footer>
+        </div>
+      </div>
+    );
   }
 }
